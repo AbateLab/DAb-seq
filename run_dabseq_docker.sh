b8958f9f29c4ef63a3061a66496ea2d061265ced
Ben Demaree <benjamindemaree@gmail.com>
2020-08-24 16:34:34 -0700

    add panel files and ab csv as command line args, totalseq support

--- run_dabseq_docker.sh
+++ run_dabseq_docker.sh
@@ -7,58 +7,71 @@
 USER=bdemaree
 
 # base input/output directory
-BASE_DIR=/drive3/testing
+BASE_DIR=/drive3/smith_lab/cohorts
 
 # cohort name and directory
-COHORT_NAME=cohort1
+COHORT_NAME=hashing_pilot_1
 COHORT_DIR=$BASE_DIR/$COHORT_NAME
 
 # sample names
-SAMPLE_NAME_1=timepoint1
-SAMPLE_NAME_2=timepoint2
+SAMPLE_NAME_1=all_tubes
+#SAMPLE_NAME_2=timepoint2
 
-# cfg file location
-CFG_FILE_DIR=/home/bdemaree/code/dab-seq/cfg
-CFG_FILE_NAME=dabseq.docker.cfg
+# directory containing files for this run
+# will be mounted into container at /dabseq/config
+RUN_DIR=/home/bdemaree/code/dab-seq/runs/totalseq_pilot_1
+
+# files in run directory needed for processing
+CFG_FILE=dabseq.hg19.cfg
+AB_CSV_FILE=ab_barcodes_hashpilot1.csv
+DNA_PANEL_FILE=UCSF_CathySmith.final
+
+# container to run
+CONTAINER=bendemaree/dab-seq:human
 
 # misc
 SLACK_TOKEN="$(cat /home/bdemaree/.slack_token)"
 
 ### BARCODING ###
 
-# note - to run using local (not GitHub) code, add the following argument to the docker call:
+# note - to run using local (not GitHub) code, add the following argument to the docker calls:
 #-v /home/bdemaree/code/dab-seq/:/dabseq/pipeline/DAb-seq/ \
 
 docker run \
 -it \
 -u $(id -u ${USER}):$(id -g ${USER}) \
--v $BASE_DIR/$COHORT_NAME/$SAMPLE_NAME_1/:/input/$COHORT_NAME/$SAMPLE_NAME_1/ \
--v $CFG_FILE_DIR/$CFG_FILE_NAME:/dabseq/config/$CFG_FILE_NAME \
 -v /home/bdemaree/code/dab-seq/:/dabseq/pipeline/DAb-seq/ \
-dab-seq $COHORT_NAME barcode /dabseq/config/$CFG_FILE_NAME \
+-v $BASE_DIR/$COHORT_NAME/$SAMPLE_NAME_1/:/input/$COHORT_NAME/$SAMPLE_NAME_1/ \
+-v $RUN_DIR:/dabseq/config/ \
+$CONTAINER $COHORT_NAME barcode /dabseq/config/$CFG_FILE \
 --sample-name $SAMPLE_NAME_1 \
---chem V1 \
+--dna-panel /dabseq/config/$DNA_PANEL_FILE \
+--ab-csv /dabseq/config/$AB_CSV_FILE \
+--chem V2 \
 --slack-token $SLACK_TOKEN
 
-docker run \
--it \
--u $(id -u ${USER}):$(id -g ${USER}) \
--v $BASE_DIR/$COHORT_NAME/$SAMPLE_NAME_2/:/input/$COHORT_NAME/$SAMPLE_NAME_2/ \
--v $CFG_FILE_DIR/$CFG_FILE_NAME:/dabseq/config/$CFG_FILE_NAME \
--v /home/bdemaree/code/dab-seq/:/dabseq/pipeline/DAb-seq/ \
-dab-seq $COHORT_NAME barcode /dabseq/config/$CFG_FILE_NAME \
---sample-name $SAMPLE_NAME_2 \
---chem V1 \
---slack-token $SLACK_TOKEN
+#docker run \
+#-it \
+#-u $(id -u ${USER}):$(id -g ${USER}) \
+#-v /home/bdemaree/code/dab-seq/:/dabseq/pipeline/DAb-seq/ \
+#-v $BASE_DIR/$COHORT_NAME/$SAMPLE_NAME_2/:/input/$COHORT_NAME/$SAMPLE_NAME_2/ \
+#-v $CFG_FILE_DIR/$CFG_FILE:/dabseq/config/$CFG_FILE \
+#-v $AB_CSV_DIR/$AB_CSV_FILE:/dabseq/config/$AB_CSV_FILE \
+#$CONTAINER $COHORT_NAME barcode /dabseq/config/$CFG_FILE \
+#--sample-name $SAMPLE_NAME_2 \
+#--dna-panel /dabseq/config/$DNA_PANEL_FILE \
+#--ab-csv /dabseq/config/$AB_CSV_FILE \
+#--chem V2 \
+#--slack-token $SLACK_TOKEN
 
 ### GENOTYPING ###
 
 docker run \
 -it \
 -u $(id -u ${USER}):$(id -g ${USER}) \
--v $COHORT_DIR/:/input/$COHORT_NAME/ \
--v $CFG_FILE_DIR/$CFG_FILE_NAME:/dabseq/config/$CFG_FILE_NAME \
 -v /home/bdemaree/code/dab-seq/:/dabseq/pipeline/DAb-seq/ \
-dab-seq $COHORT_NAME genotype /dabseq/config/$CFG_FILE_NAME \
+-v $COHORT_DIR/:/input/$COHORT_NAME/ \
+-v $RUN_DIR:/dabseq/config/ \
+$CONTAINER $COHORT_NAME genotype /dabseq/config/$CFG_FILE \
+--dna-panel /dabseq/config/$DNA_PANEL_FILE \
 --slack-token $SLACK_TOKEN
-
